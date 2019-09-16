108827d9ed3f6681848edb8bd29810891f8aef49
Yone <yone.moreno101@alu.ulpgc.es>
2019-09-16 15:13:05 +0100

    Initial commit

---
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -0,0 +1,13 @@
+package com.platzi.javatests.util;
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+
+        String result = StringUtil.repeat("hola",3);
+        System.out.println(result);
+
+        String result2 = StringUtil.repeat("hola",1);
+        System.out.println(result2);
+
+    }
+}
