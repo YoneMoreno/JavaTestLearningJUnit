024e6275b48c04f1a324d2818622fe3dd6711bc3
Yone <yone.moreno101@alu.ulpgc.es>
2019-09-16 15:24:34 +0100

    Creating unit tests

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -4,10 +4,15 @@ public class StringUtilTest {
     public static void main(String[] args) {
 
         String result = StringUtil.repeat("hola",3);
-        System.out.println(result);
+        assertEquals(result, "holaholahola");
 
         String result2 = StringUtil.repeat("hola",1);
-        System.out.println(result2);
+        assertEquals(result2, "hola");
+    }
 
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to: " + expected);
+        }
     }
 }
