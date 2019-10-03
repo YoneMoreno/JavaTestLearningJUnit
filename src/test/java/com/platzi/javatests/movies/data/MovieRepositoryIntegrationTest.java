9195fd3202d8267b647bd1150cb07a2b4ea58dd9
Yone <yonem9@gmail.com>
2019-10-03 21:57:43 +0100

    Add findMovieById

--- src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
+++ src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
@@ -2,13 +2,17 @@ package com.platzi.javatests.movies.data;
 
 import com.platzi.javatests.movies.model.Genre;
 import com.platzi.javatests.movies.model.Movie;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -16,20 +20,40 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class MovieRepositoryIntegrationTest {
-    @Test
-    public void loadAllMovies() throws SQLException {
 
-        DriverManagerDataSource dataSource =
-                new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
+    private MovieRepository movieRepositoryJdbc;
+    private DriverManagerDataSource dataSource;
+
+    @Before
+    public void setUp() throws Exception {
+
+        dataSource = new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
 
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        MovieRepositoryJdbc movieRepositoryJdbc = new MovieRepositoryJdbc(jdbcTemplate);
+        movieRepositoryJdbc = new MovieRepositoryJdbc(jdbcTemplate);
+    }
+
+    @Test
+    public void loadAllMovies() throws SQLException {
+
         Collection<Movie> movies = movieRepositoryJdbc.findAll();
         assertThat(movies, is(Arrays.asList(
                 new Movie(1, "Dark Knight", 152, Genre.ACTION),
                 new Movie(2, "Memento", 113, Genre.THRILLER),
                 new Movie(3, "Matrix", 136, Genre.ACTION)
         )));
+    }
+
+    @Test
+    public void loadMovieById() {
+        Movie movie = movieRepositoryJdbc.findById(2);
+        assertThat(movie, is(new Movie(2, "Memento", 113, Genre.THRILLER)));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        final Statement s = dataSource.getConnection().createStatement();
+        s.execute("drop all objects delete files");
     }
 }
