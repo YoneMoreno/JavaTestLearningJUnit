58272c13cc12287790d7bb28345c263e7c06ce25
Yone <yonem9@gmail.com>
2019-10-03 21:29:09 +0100

    Add integration test to know if we can findAll movies from DB

---
+++ src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJdbc.java
@@ -0,0 +1,41 @@
+package com.platzi.javatests.movies.data;
+
+import com.platzi.javatests.movies.model.Genre;
+import com.platzi.javatests.movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class MovieRepositoryJdbc implements MovieRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MovieRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+        return jdbcTemplate.query("select * from movies", movieRowMapper);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+
+    private static RowMapper<Movie> movieRowMapper = (resultSet, i) -> new Movie(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getInt("minutes"),
+            Genre.valueOf(resultSet.getString("genre"))
+    );
+}
